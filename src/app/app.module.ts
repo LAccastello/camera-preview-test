54ce619661da64e66f180510c5b7a08f74eba27e
Lucas Accastello <accastellolucas@gmail.com>
2020-11-20 15:17:52 -0300

    first upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import '@capacitor-community/camera-preview';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,8 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { CameraPreview } = Plugins;
+// import { CameraPreviewOptions } from '@capacitor-community/camera-preview';
+import { CameraPreviewOptions } from '@capacitor-community/camera-preview';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   constructor() {}
 
+  ngOnInit() {
+    CameraPreview.start({ parent: 'cameraPreview' })
+      .then(() => {
+        const video = <HTMLVideoElement>document.getElementById('video');
+        video.setAttribute('playsinline', '');
+        video.play();
+      })
+      .catch((err) => {
+        alert(JSON.stringify(err));
+      });
+  }
+
+  capture() {
+    CameraPreview.capture().then((a) => console.log(JSON.stringify(a)));
+  }
 }
